e676aa4b68b3d7d5836dc09c89007b243b0741ee
Sergej <malykh-88@mail.ru>
2017-06-05 16:57:43 +0700

    Merge branch 'master' of https://github.com/MalykhS/Academits
    # Conflicts:
    #	Shapes/src/ru/academits/malykh/main/Main.java
    #	Shapes/src/ru/academits/malykh/shapes/Circle.java
    #	Shapes/src/ru/academits/malykh/shapes/Rectangle.java
    #	Shapes/src/ru/academits/malykh/shapes/Shape.java
    #	Shapes/src/ru/academits/malykh/shapes/Square.java
    #	Shapes/src/ru/academits/malykh/shapes/Triangle.java
    #	Shapes/src/ru/academits/malykh/shapes/Utils.java

--- Vector/src/ru/academits/malykh/main/Main.java
+++ Vector/src/ru/academits/malykh/main/Main.java
@@ -2,12 +2,16 @@ package ru.academits.malykh.main;
 
 import ru.academits.malykh.vector.Vector;
 
+
 public class Main {
     public static void main(String[] args) {
-        double[] array = new double[]{1, 2, 3, 4, 5};
-        double[] array1 = new double[]{2, 3, 4, 5, 6};
-        Vector vector = new Vector(array);
-        Vector vector1 = new Vector(array1);
-        System.out.println(Vector.getSum(vector, vector1));
+        Vector vector1 = new Vector(1, 2, 3, 4, 5, 1);
+        Vector vector2 = new Vector(2, 3, 4, 5, 6);
+        Vector vector3 = new Vector();
+
+        System.out.println(Vector.getAddition(vector1, vector2));
+        //System.out.println(Vector.getAddition(vector1, vector2));
+
+
     }
 }
--- Vector/src/ru/academits/malykh/vector/Vector.java
+++ Vector/src/ru/academits/malykh/vector/Vector.java
@@ -4,34 +4,42 @@ import java.util.Arrays;
 
 public class Vector {
     private double[] array;
-    private int size;
+    private int n;
 
-    public Vector() {}
-
-    public Vector(int size) {
-        this.size = size;
+    public Vector(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("n is 0 or less than 0");
+        }
+        this.n = n;
     }
 
     public Vector(Vector vector) {
         this.array = vector.array;
-        this.size = vector.size;
+        this.n = vector.n;
     }
 
-    public Vector(double[] array) {
+    public Vector(double... array) {
         this.array = array;
     }
 
-    public Vector(int size, double[] array) {
-        this.size = size;
-        this.array = array;
+    public Vector(int n, double[] array) {
+        if (array.length < n) {
+            for (int i = 0; i < array.length; i++) {
+                array[i] = 0;
+            }
+        }
     }
 
     public int getSize() {
-        return size;
+        return n;
     }
 
-    public void setSize(int size) {
-        this.size = size;
+    public double getElement(int index) {
+        return this.array[index];
+    }
+
+    public void setElement(int index, double value) {
+        this.array[index] = value;
     }
 
     public int getLength() {
@@ -41,17 +49,93 @@ public class Vector {
         return array.length;
     }
 
-    public static Vector getSum(Vector vector1, Vector vector2) {
-        if (vector1.size != vector2.size) {
-            System.err.println("Разный размер!");
+    public Vector getAdd(Vector v1, Vector v2) {
+        if (v1.getLength() == v2.getLength()) {
+            for (int i = 0; i < v1.getLength(); i++) {
+                v1.array[i] += v2.array[i];
+            }
         }
+        return v1;
+    }
 
-        return new Vector(vector1.size + vector2.size);
+    public Vector getSub(Vector v1, Vector v2) {
+        if (v1.getLength() == v2.getLength()) {
+            for (int i = 0; i < v1.getLength(); i++) {
+                v1.array[i] -= v2.array[i];
+            }
+        }
+        return v1;
+    }
+
+    public Vector getMultiplication(Vector v1, double number) {
+        for (int i = 0; i < v1.getLength(); i++) {
+            v1.array[i] *= number;
+        }
+        return v1;
+    }
+
+    public Vector getReverse(Vector v1) {
+        for (int i = 0; i < v1.getLength(); i++) {
+            v1.array[i] *= -1;
+        }
+        return v1;
+    }
+
+
+    public static Vector getAddition(Vector v1, Vector v2) {
+        Vector v3 = new Vector(v1);
+        if (v1.getLength() == v2.getLength()) { //|| (v1.getLength() > v2.getLength())) {
+            for (int i = 0; i < v1.getLength(); i++) {
+                v3.array[i] = v1.array[i] + v2.array[i];
+            }
+        }
+        return v3;
+    }
+
+    public static Vector getSubtraction(Vector v1, Vector v2) {
+        Vector v3 = new Vector(v1);
+        if (v1.getLength() == v2.getLength()) {
+            for (int i = 0; i < v3.getLength(); i++) {
+                v3.array[i] = v1.array[i] - v2.array[i];
+            }
+        }
+        return v3;
+    }
+
+    public static Vector getScalarMultiplication(Vector v1, Vector v2) {
+        Vector v3 = new Vector(v1);
+        for (int i = 0; i < v3.getLength(); i++) {
+            v3.array[i] = v1.array[i] * v2.array[i];
+        }
+        return v3;
     }
 
     @Override
-
     public String toString() {
         return Arrays.toString(array);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || o.getClass() != this.getClass()) {
+            return false;
+        }
+
+        Vector vector = (Vector) o;
+
+        if (n != vector.n) {
+            return false;
+        }
+        return Arrays.equals(array, vector.array);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Arrays.hashCode(array);
+        result = 31 * result + n;
+        return result;
     }
 }
