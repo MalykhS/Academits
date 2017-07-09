8f1ec9d0ec5fcb49060fa03a08bc3098fb9dce0f
admin <malykh-88@mail.ru>
2017-07-09 23:22:22 +0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Shapes/src/ru/academits/malykh/main/Main.java
    #	Shapes/src/ru/academits/malykh/shapes/Utils.java

--- Sort/src/ru/academits/malykh/sort/main/Sort.java
+++ Sort/src/ru/academits/malykh/sort/main/Sort.java
@@ -1,60 +1,55 @@
 package ru.academits.malykh.sort.main;
 
-import java.io.*;
-import java.util.Scanner;
+import java.util.ArrayList;
 
-public class Sort {
-    private static void printIllegalFlag(String[] args) {
-        if (!args[2].equals("-i") && (!args[2].equals("-s"))) {
-            System.out.println("Not correct first argument! You must enter -i or -s");
-        }
-        if (!args[3].equals("-a") && (!args[3].equals("-d"))) {
-            System.out.println("Not correct second argument! You must enter -a or -d!");
+class Sort {
+    static void getAscendingSort(ArrayList<Integer> list) {
+        for (int i = 1; i < list.size(); i++) {
+            int temp = list.get(i);
+            int j = i - 1;
+
+            while (j >= 0 && list.get(j) > temp) {
+                list.set((j + 1), list.get(j));
+                j--;
+            }
+            list.set((j + 1), temp);
         }
     }
 
-    public static void main(String[] args) {
-        if (args.length != 4) {
-            System.out.println("You must enter 4 arguments!");
-            return;
-        }
-        try (Scanner scanner = new Scanner(new FileInputStream(args[0]));
-             PrintWriter writer = new PrintWriter(args[1])) {
+    static void getDescendingSort(ArrayList<Integer> list) {
+        for (int i = 1; i < list.size(); i++) {
+            int temp = list.get(i);
+            int j = i - 1;
 
-            switch (args[2]) {
-                case "-i":
-                    switch (args[3]) {
-                        case "-a":
-                            Utils.writeAscendingSortNumbersFile(scanner, writer);
-                            break;
-                        case "-d":
-                            Utils.writeDescendingSortNumbersFile(scanner, writer);
-                            break;
-                        default:
-                            printIllegalFlag(args);
-                            break;
-                    }
-
-                    break;
-                case "-s":
-                    switch (args[3]) {
-                        case "-a":
-                            Utils.writeAscendingSortStringsFile(scanner, writer);
-                            break;
-                        case "-d":
-                            Utils.writeDescendingSortStringsFile(scanner, writer);
-                            break;
-                        default:
-                            printIllegalFlag(args);
-                            break;
-                    }
-                    break;
-                default:
-                    printIllegalFlag(args);
-                    break;
+            while (j >= 0 && list.get(j) < temp) {
+                list.set((j + 1), list.get(j));
+                j--;
             }
-        } catch (FileNotFoundException ex) {
-            System.out.println("File is not found!");
+            list.set((j + 1), temp);
+        }
+    }
+
+    static void ascendingSort(ArrayList<String> list) {
+        String str;
+        int j;
+        for (int i = 0; i < list.size(); i++) {
+            str = list.get(i);
+            for (j = i - 1; j >= 0 && list.get(j).compareTo(str) > 0; j--) {
+                list.set((j + 1), list.get(j));
+            }
+            list.set((j + 1), str);
+        }
+    }
+
+    static void descendingSort(ArrayList<String> list) {
+        String str;
+        int j;
+        for (int i = 0; i < list.size(); i++) {
+            str = list.get(i);
+            for (j = i - 1; j >= 0 && list.get(j).compareTo(str) < 0; j--) {
+                list.set((j + 1), list.get(j));
+            }
+            list.set((j + 1), str);
         }
     }
 }
--- Sort/src/ru/academits/malykh/sort/main/Utils.java
+++ Sort/src/ru/academits/malykh/sort/main/Utils.java
@@ -1,91 +1,51 @@
 package ru.academits.malykh.sort.main;
 
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 class Utils {
-    private static void getAscendingSort(ArrayList<Integer> list) {
-        for (int i = 1; i < list.size(); i++) {
-            int temp = list.get(i);
-            int j = i - 1;
 
-            while (j >= 0 && list.get(j) > temp) {
-                list.set((j + 1), list.get(j));
-                j--;
-            }
-            list.set((j + 1), temp);
+    private static void readNumbersFile(Scanner scanner, ArrayList<Integer> list) {
+        while (scanner.hasNext()) {
+            list.add(scanner.nextInt());
         }
     }
 
-    private static void getDescendingSort(ArrayList<Integer> list) {
-        for (int i = 1; i < list.size(); i++) {
-            int temp = list.get(i);
-            int j = i - 1;
-
-            while (j >= 0 && list.get(j) < temp) {
-                list.set((j + 1), list.get(j));
-                j--;
-            }
-            list.set((j + 1), temp);
+    private static void readStringsFile(Scanner scanner, ArrayList<String> list) {
+        while (scanner.hasNext()) {
+            list.add(scanner.nextLine());
         }
     }
 
-    private static void ascendingSort(ArrayList<String> list) {
-        String str;
-        int j;
-        for (int i = 0; i < list.size(); i++) {
-            str = list.get(i);
-            for (j = i - 1; j >= 0 && list.get(j).compareTo(str) > 0; j--) {
-                list.set((j + 1), list.get(j));
-            }
-            list.set((j + 1), str);
-        }
-    }
-
-    private static void descendingSort(ArrayList<String> list) {
-        String str;
-        int j;
-        for (int i = 0; i < list.size(); i++) {
-            str = list.get(i);
-            for (j = i - 1; j >= 0 && list.get(j).compareTo(str) < 0; j--) {
-                list.set((j + 1), list.get(j));
-            }
-            list.set((j + 1), str);
+    private static <T> void writeFile(PrintWriter writer, ArrayList<T> list) {
+        for (T e : list) {
+            writer.println(e);
         }
     }
 
     static void writeAscendingSortNumbersFile(Scanner scanner, PrintWriter writer) {
-        ArrayList<Integer> list = new ArrayList<>();
-        while (scanner.hasNextInt()) {
-            list.add(scanner.nextInt());
-        }
 
-        Utils.getAscendingSort(list);
-        for (int e : list) {
-            writer.println(e);
-        }
+        ArrayList<Integer> list = new ArrayList<>();
+        Utils.readNumbersFile(scanner, list);
+
+        Sort.getAscendingSort(list);
+        Utils.writeFile(writer, list);
     }
 
     static void writeDescendingSortNumbersFile(Scanner scanner, PrintWriter writer) {
         ArrayList<Integer> list = new ArrayList<>();
-        while (scanner.hasNextInt()) {
-            list.add(scanner.nextInt());
-        }
+        Utils.readNumbersFile(scanner, list);
 
-        Utils.getDescendingSort(list);
-        for (int e : list) {
-            writer.println(e);
-        }
+        Sort.getDescendingSort(list);
+        Utils.writeFile(writer, list);
     }
 
     static void writeAscendingSortStringsFile(Scanner scanner, PrintWriter writer) {
         ArrayList<String> list = new ArrayList<>();
-        while (scanner.hasNextLine()) {
-            list.add(scanner.nextLine());
-        }
+        Utils.readStringsFile(scanner, list);
 
-        Utils.ascendingSort(list);
+        Sort.ascendingSort(list);
         for (String e : list) {
             writer.println(e);
         }
@@ -93,13 +53,9 @@ class Utils {
 
     static void writeDescendingSortStringsFile(Scanner scanner, PrintWriter writer) {
         ArrayList<String> list = new ArrayList<>();
-        while (scanner.hasNextLine()) {
-            list.add(scanner.nextLine());
-        }
+        Utils.readStringsFile(scanner, list);
 
-        Utils.descendingSort(list);
-        for (String e : list) {
-            writer.println(e);
-        }
+        Sort.descendingSort(list);
+        Utils.writeFile(writer, list);
     }
 }
